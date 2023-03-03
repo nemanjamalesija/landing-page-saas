4b6243c239eadf322482a65a1fdce7543b1c9863
Nemanja Malesija <malesija13@gmail.com>
2023-03-03 18:18:25 +0100

    Create nav basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,24 @@ import React from 'react';
 import moi from './images/moi.png';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <nav className="nav">
+        <div className="nav__logo--container">
+          <div className="nav__logo__box">
+            <img src={moi} alt="user" />
+          </div>
+          <p className="nav__logo--text">Bekenbauers</p>
+        </div>
+        <ul className="nav__list">
+          <li className="nav__list--item">Home</li>
+          <li className="nav__list--item">About</li>
+          <li className="nav__list--item">Produts</li>
+          <li className="nav__list--item">Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
